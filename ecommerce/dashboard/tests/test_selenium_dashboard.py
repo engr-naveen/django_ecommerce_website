50e1862288c48e0d3f37078eedcc7659ecbb7944
Naveen-MAC <56880985+engr-naveen@users.noreply.github.com>
2025-06-24 13:57:41 +0530

    refactor code - ecommerce/dashboard/tests/test_selenium_dashboard.py

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -17,22 +17,22 @@ def test_dashboad_admin_login(live_server,db_fixture_setup,chrome_browser_instan
 
     browser = chrome_browser_instance
     browser.get(("%s%s" % (live_server.url,"/admin/login/")))
-    
+    time.sleep(delay)
+
     user_name = browser.find_element(By.NAME,"username")
     user_password = browser.find_element(By.NAME,"password")
     log_in_submit = browser.find_element(By.XPATH,'//input[@value="Log in"]')
-
-    time.sleep(delay)
+    
     user_name.send_keys("admin")
     user_password.send_keys("password")
     log_in_submit.send_keys(Keys.RETURN)
-
     time.sleep(delay)
+    
     # assert cerate_admin_user.__str__() == "admin" 
     assert "Site administration" in browser.page_source
 
     log_out_submit = browser.find_element(By.XPATH,'//button[contains(.,"Log out")]')
     log_out_submit.send_keys(Keys.RETURN)
-
     time.sleep(delay)
+    
     assert "Logged out" in browser.page_source
