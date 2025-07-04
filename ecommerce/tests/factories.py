c3f14a92a6a02e4b31fa41aabda1643f6fdc0400
Naveen-MAC <56880985+engr-naveen@users.noreply.github.com>
2025-07-04 21:15:20 +0530

    added tests for product type and brand

--- ecommerce/tests/factories.py
+++ ecommerce/tests/factories.py
@@ -22,6 +22,7 @@ class CategoryFactory(factory.django.DjangoModelFactory):
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Product
+        skip_postgeneration_save = True
 
     web_id = factory.Sequence(lambda n: "web_id_%d" % n)
     slug = fake.lexify(text="prod_slug_??????")
@@ -41,6 +42,41 @@ class ProductFactory(factory.django.DjangoModelFactory):
                 self.category.add(cat)
 
 
+class ProductTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.ProductType
+
+    name = factory.Sequence(lambda n: "type_%d" % n)
+
+
+class BrandFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Brand
+
+    name = factory.Sequence(lambda n: "brand_%d" % n)
+
+
+class ProductInventoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.ProductInventory
+
+    sku = factory.Sequence(lambda n: "sku_%d" % n)
+    upc = factory.Sequence(lambda n: "upc_%d" % n)
+
+    product = factory.SubFactory(ProductFactory)
+    product_type = factory.SubFactory(ProductTypeFactory)
+    brand = factory.SubFactory(BrandFactory)
+
+    is_active = 1
+    retail_price = 97
+    store_price = 92
+    sale_price = 46
+    weight = 987
+
+
 # Makes it avaliable
 register(CategoryFactory)
 register(ProductFactory)
+register(ProductTypeFactory)
+register(BrandFactory)
+register(ProductInventoryFactory)
