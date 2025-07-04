de5a867e368ea0ab2caf2418e27e6cb8cac13759
Naveen-MAC <56880985+engr-naveen@users.noreply.github.com>
2025-07-04 18:33:25 +0530

    created product table and their tests

--- ecommerce/tests/factories.py
+++ ecommerce/tests/factories.py
@@ -19,5 +19,28 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     slug = factory.Sequence(lambda n: "cat_slug_%d" % n)
 
 
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Product
+
+    web_id = factory.Sequence(lambda n: "web_id_%d" % n)
+    slug = fake.lexify(text="prod_slug_??????")
+    name = fake.lexify(text="prod_name_??????")
+    description = fake.text()
+    is_active = True
+    created_at = "2021-09-04 22:14:18.279092"
+    updated_at = "2021-09-04 22:14:18.279092"
+
+    @factory.post_generation
+    def category(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+
+        if extracted:
+            for cat in extracted:
+                self.category.add(cat)
+
+
 # Makes it avaliable
 register(CategoryFactory)
+register(ProductFactory)
